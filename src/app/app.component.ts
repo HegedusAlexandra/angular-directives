a52a601d58f5c8ec0fb023ae6d60dcb567335498
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-04-29 17:38:48 +0200

    searchbar hidden and removed ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
+import { CommonModule } from '@angular/common';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavComponent],
+  standalone: true, 
+  imports: [RouterOutlet,NavComponent,CommonModule,AlertModule,
+    ModalModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-directives';
+
+  isSearchBarVisible:boolean = false;
+
+  toggleSearchbar():void{
+    this.isSearchBarVisible = !this.isSearchBarVisible
+  }
 }
