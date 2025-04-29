b060b86c821ae0104109bc699233ec5f1428fec2
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-04-29 17:53:13 +0200

    ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,45 @@ import { NavComponent } from './common/nav/nav.component';
 import { CommonModule } from '@angular/common';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { Ticket } from './model/ticket';
 
 @Component({
   selector: 'app-root',
-  standalone: true, 
-  imports: [RouterOutlet,NavComponent,CommonModule,AlertModule,
-    ModalModule],
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, CommonModule, AlertModule, ModalModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-directives';
 
-  isSearchBarVisible:boolean = false;
+  isSearchBarVisible: boolean = false;
 
-  toggleSearchbar():void{
-    this.isSearchBarVisible = !this.isSearchBarVisible
+  toggleSearchbar(): void {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
   }
+
+  tickets: Ticket[] = [
+    {
+      id: 1,
+      checked: false,
+      flightNumber: 'wa3421',
+      seat: 'D3',
+      service: 'economy',
+    },
+    {
+      id: 2,
+      checked: false,
+      flightNumber: 'wa3421',
+      seat: 'E3',
+      service: 'economy',
+    },
+    {
+      id: 3,
+      checked: false,
+      flightNumber: 'wa3421',
+      seat: 'F3',
+      service: 'economy',
+    },
+  ];
 }
