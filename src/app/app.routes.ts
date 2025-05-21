6cb6f5fba127daf3b5cdda8311030d8d82d0438e
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-05-21 11:48:47 +0200

    added navigation components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { TicketsComponent } from './page/tickets/tickets.component';
+import { AboutComponent } from './page/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'tickets',
+        component: TicketsComponent
+    },
+    {
+        path:'about',
+        component: AboutComponent
+    },
+    {
+        path:'**',
+        redirectTo: ''
+    },
+];
