f258d216e25076639a52da363ff60fd3c5fcd3d4
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-05-21 15:55:58 +0200

    added router outlet and move tickets page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { NavComponent } from './common/nav/nav.component';
 import { CommonModule } from '@angular/common';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { Ticket } from './model/ticket';
-import {
-  BtnGroupComponent,
-  IBtn,
-} from './common/btn-group/btn-group.component';
+import { BtnGroupComponent } from './common/btn-group/btn-group.component';
 import { BooleanPipe } from './pipe/boolean.pipe';
 import { ArrayFilterPipe } from './pipe/array-filter.pipe';
-import { TicketService } from './service/ticket.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -24,92 +19,9 @@ import { TicketService } from './service/ticket.service';
     ModalModule,
     BooleanPipe,
     ArrayFilterPipe,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-directives';
-
-  ticketService: TicketService = inject(TicketService);
-
-  isSearchBarVisible: boolean = false;
-
-  toggleSearchbar(): void {
-    this.isSearchBarVisible = !this.isSearchBarVisible;
-  }
-
- tickets: Ticket[] = [
-    {
-      id: 1,
-      checked: false,
-      flightNumber: 'wa3421',
-      seat: 'A5',
-      service: 'economy',
-    },
-    {
-      id: 2,
-      checked: true,
-      flightNumber: 'wk3421',
-      seat: 'E3',
-      service: 'economy',
-    },
-    {
-      id: 3,
-      checked: false,
-      flightNumber: 'wa3621',
-      seat: 'F3',
-      service: 'business',
-    },
-  ]; 
-
-  btnGroup: IBtn[] = [
-    { name: 'show', type: 'info', icon: 'fa fa-eye' },
-    { name: 'remove', type: 'danger', icon: 'fa fa-trash' },
-  ];
-
-  tickets$ = this.ticketService.list$; 
-
-  ngOnInit(): void {
-    this.ticketService.dispatch('getAll');
-
-    /* const testTicket: Ticket = {
-      id: 0,
-      checked: false,
-      flightNumber: 'wa3421',
-      seat: 'A5',
-      service: 'economy',
-    };
-
-    this.ticketService
-      .create(testTicket)
-      .subscribe((ticket) => console.log(ticket));
-
-    this.ticketService
-      .getAll()
-      .subscribe((tickets) => console.log(tickets)); */
-  }
-
-/*   onGroupClick(details: { name: string; data: any }) {
-    if (details.name === 'remove') {
-      const index = this.tickets.findIndex((ticket) => ticket === details.data);
-      if (index > -1) {
-        this.tickets.splice(index, 1);
-      }
-    }
-  } */
-
-  onGroupClick(details: {name: string; data: any}) {
-      
-    switch(details.name) {
-      case 'remove':
-        this.ticketService.dispatch('delete', (details.data as Ticket));
-        break;
-      case 'show':
-        this.ticketService.dispatch('get', (details.data as Ticket).id);
-        break
-    }
-  }
-
-  filterPhrase: string = '';
-}
+export class AppComponent {}
--- src/app/page/tickets/tickets.component.ts
+++ src/app/page/tickets/tickets.component.ts
@@ -1,11 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  BtnGroupComponent,
+  IBtn,
+} from '../../common/btn-group/btn-group.component';
+import { Ticket } from '../../model/ticket';
+import { TicketService } from '../../service/ticket.service';
+import { CommonModule } from '@angular/common';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NavComponent } from '../../common/nav/nav.component';
+import { ArrayFilterPipe } from '../../pipe/array-filter.pipe';
+import { BooleanPipe } from '../../pipe/boolean.pipe';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [
+    BtnGroupComponent,
+    CommonModule,
+    AlertModule,
+    ModalModule,
+    BooleanPipe,
+    ArrayFilterPipe,
+  ],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.scss'
+  styleUrl: './tickets.component.scss',
 })
 export class TicketsComponent {
+  title = 'angular-directives';
 
+  ticketService: TicketService = inject(TicketService);
+
+  isSearchBarVisible: boolean = false;
+
+  toggleSearchbar(): void {
+    this.isSearchBarVisible = !this.isSearchBarVisible;
+  }
+
+  tickets: Ticket[] = [
+    {
+      id: 1,
+      checked: false,
+      flightNumber: 'wa3421',
+      seat: 'A5',
+      service: 'economy',
+    },
+    {
+      id: 2,
+      checked: true,
+      flightNumber: 'wk3421',
+      seat: 'E3',
+      service: 'economy',
+    },
+    {
+      id: 3,
+      checked: false,
+      flightNumber: 'wa3621',
+      seat: 'F3',
+      service: 'business',
+    },
+  ];
+
+  btnGroup: IBtn[] = [
+    { name: 'show', type: 'info', icon: 'fa fa-eye' },
+    { name: 'remove', type: 'danger', icon: 'fa fa-trash' },
+  ];
+
+  tickets$ = this.ticketService.list$;
+
+  ngOnInit(): void {
+    this.ticketService.dispatch('getAll');
+
+    /* const testTicket: Ticket = {
+      id: 0,
+      checked: false,
+      flightNumber: 'wa3421',
+      seat: 'A5',
+      service: 'economy',
+    };
+
+    this.ticketService
+      .create(testTicket)
+      .subscribe((ticket) => console.log(ticket));
+
+    this.ticketService
+      .getAll()
+      .subscribe((tickets) => console.log(tickets)); */
+  }
+
+  /*   onGroupClick(details: { name: string; data: any }) {
+    if (details.name === 'remove') {
+      const index = this.tickets.findIndex((ticket) => ticket === details.data);
+      if (index > -1) {
+        this.tickets.splice(index, 1);
+      }
+    }
+  } */
+
+  onGroupClick(details: { name: string; data: any }) {
+    switch (details.name) {
+      case 'remove':
+        this.ticketService.dispatch('delete', details.data as Ticket);
+        break;
+      case 'show':
+        this.ticketService.dispatch('get', (details.data as Ticket).id);
+        break;
+    }
+  }
+
+  filterPhrase: string = '';
 }
