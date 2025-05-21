e92ae53ffa7930d81a3faf90f9da6ee7cebb5126
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-05-21 21:33:03 +0200

    data update for tickets

--- src/app/ticket-editor/ticket-editor.component.ts
+++ src/app/ticket-editor/ticket-editor.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TicketService } from '../service/ticket.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../model/ticket';
+
 @Component({
   selector: 'app-ticket-editor',
   imports: [CommonModule,FormsModule],
@@ -11,8 +13,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class TicketEditorComponent {
   ticketService = inject(TicketService);
-
+  router = inject(Router)
   route = inject(ActivatedRoute);
+
   id = this.route.snapshot.paramMap.get('id');
 
   ticket$ = this.ticketService.one$;
@@ -21,5 +24,9 @@ export class TicketEditorComponent {
     if (this.id) {
       this.ticketService.dispatch('get', this.id);
     }
+  }
+
+  onUpdate(ticket: Ticket):void{
+    this.ticketService.update(ticket).subscribe(updatedTicket => this.router.navigate(['/tickets']))
   }
 }
