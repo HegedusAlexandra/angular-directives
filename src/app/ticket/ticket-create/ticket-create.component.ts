35d3633761d8c32e8d2fd607971d1673678fd182
AlexaHegedus <alexandra.hegedus@vamosoft.hu>
2025-05-22 15:00:56 +0200

    formcontrol

--- src/app/ticket/ticket-create/ticket-create.component.ts
+++ src/app/ticket/ticket-create/ticket-create.component.ts
@@ -1,12 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Ticket } from '../../model/ticket';
 import { TicketService } from '../../service/ticket.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ticket-create',
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './ticket-create.component.html',
   styleUrl: './ticket-create.component.scss',
 })
@@ -14,9 +19,39 @@ export class TicketCreateComponent {
   ticketService = inject(TicketService);
   ticket = new Ticket();
   form: FormGroup = new FormGroup({
-    flightNumber: new FormControl<string>(''),
-    service: new FormControl<string>(''),
-    seat: new FormControl<string>(''),
+    flightNumber: new FormControl<string>(
+      {
+        value: '',
+        disabled: false,
+      },
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z]{2}[0-9]{4}$/),
+        ],
+      }
+    ),
+    service: new FormControl<string>(
+      {
+        value: '',
+        disabled: false,
+      },
+      {
+        validators: [Validators.required],
+      }
+    ),
+    seat: new FormControl<string>(
+      {
+        value: '',
+        disabled: false,
+      },
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z]{2}[0-9]{4}$/),
+        ],
+      }
+    ),
     checked: new FormControl<boolean>(false),
   });
 }
